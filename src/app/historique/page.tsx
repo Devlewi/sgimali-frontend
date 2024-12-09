e7319671b5de2a9709d9178e97c4e1cb909902d3
devlewi <lewimarcjean3@gmail.com>
2024-12-09 12:47:52 +0000

    finalisation page historique

--- src/app/historique/page.tsx
+++ src/app/historique/page.tsx
@@ -1,5 +1,6 @@
 import HeaderPageSection from "@/components/HeaderPageSection";
 import SectionTitle from "@/components/SectionTitle";
+import SkeletonHeaderPageSection from "@/components/skeleton/SkeletonHeaderPageSection";
 import SkeletonHistorique from "@/components/skeleton/SkeletonHistorique";
 import { Metadata } from "next";
 import Image from "next/image";
@@ -92,13 +93,14 @@ export default async function Historique() {
 
   if (!historique) {
     return (
-      <></>
+      <SkeletonHeaderPageSection/>
     );
   }
   
     
   return (
     <div>
+            
       <HeaderPageSection title={historique.title} />      
       <section style={{ padding: "39px 0" }}>
         <div className="container">
--- src/components/skeleton/SkeletonHeaderPageSection.tsx
+++ src/components/skeleton/SkeletonHeaderPageSection.tsx
@@ -4,26 +4,19 @@ import "react-loading-skeleton/dist/skeleton.css";
 
 const SkeletonHeaderPageSection: React.FC = () => {
   return (
-    <section
-      className="no-padding sh-company-history skeleton"
-      style={{ paddingTop: "4rem", paddingBottom: "4rem" }}
-    >
-      <div className="sub-header">
-        {/* Skeleton pour le titre */}
-        <Skeleton
-          width="50%"
-          height={32}
-          style={{ marginBottom: "1rem", borderRadius: "8px", animationDuration: "1.2s" }}
-        />
-
-        {/* Skeleton pour la breadcrumb */}
-        <Skeleton
-          width="80%"
-          height={24}
-          style={{ borderRadius: "8px", animationDuration: "1.2s" }}
-        />
-      </div>
-    </section>
+    <div style={{ width: "100%", maxWidth: "1931px", height: "388px", margin: "0 auto" }}>
+      <Skeleton
+        height="100%"
+        width="100%"
+        style={{
+          borderRadius: "8px",
+          animationDuration: "2s", // Durée de l'animation augmentée
+          animationTimingFunction: "ease-in-out", // Animation plus fluide
+          animationDirection: "alternate", // Va-et-vient
+          animationIterationCount: "infinite", // Animation continue
+        }}
+      />
+    </div>
   );
 };
 
