a427c7723f5bf7a90dc4667f4a53b2ff3e3a6007
devlewi <lewimarcjean3@gmail.com>
2024-12-12 13:32:59 +0000

    finalisation de la page souscription

--- src/app/souscription/page.tsx
+++ src/app/souscription/page.tsx
@@ -1,327 +1,117 @@
 import HeaderPageSection from "@/components/HeaderPageSection";
 import SectionTitle from "@/components/SectionTitle";
+import SkeletonHeaderPageSection from "@/components/skeleton/SkeletonHeaderPageSection";
+import SkeletonTemplatePages from "@/components/skeleton/SkeletonTemplatePages";
+import SubscriptionForm from "@/components/SubscriptionForm";
+import { Metadata } from "next";
+import Image from "next/image";
 
-export const metadata = {
+// Type de données
+type SouscriptionData = {
+  title: string;
+  description: string;
+  image: string;
+  slug: string;
+};
+
+interface Page {
+  title: {
+    rendered: string;
+  };
+  content: {
+    rendered: string;
+  };
+  featured_image_url?: string;
+  slug: string;
+}
+
+export const metadata: Metadata = {
   title: "SOUSCRIPTION | SGI Mali",
-  description:
-    "Souscrivez à nos offres et services pour bénéficier des meilleures solutions proposées par SGI Mali.",
+  description: "Page Nous Contacter SGI Mali",
   icons: {
-    icon: "/favicon.ico",
-    apple: "/apple-touch-icon.png",
-    shortcut: "/apple-touch-icon.png",
+    icon: "/favicon.ico", // Icône générale pour le site
+    apple: "/apple-touch-icon.png", // Icône pour les appareils Apple
+    shortcut: "/apple-touch-icon.png", // Icône pour raccourci de navigateur
   },
-  openGraph: {
-    title: "SOUSCRIPTION | SGI Mali",
-    description:
-      "Découvrez les offres de souscription et choisissez celle qui répond à vos besoins. SGI Mali vous offre des solutions adaptées.",
-    url: "https://sgimali-frontend.vercel.app/souscription",
-    siteName: "SGI Mali",
-    images: [
-      {
-        url: "https://sgimali-frontend.vercel.app/images/logo-og.png",
-        width: 120,
-        height: 120,
-        alt: "Logo SGI Mali",
-      },
-    ],
-    locale: "fr_FR",
-    type: "website",
-  },
-  twitter: {
-    card: "summary_large_image",
-    title: "SOUSCRIPTION | SGI Mali",
-    description:
-      "Souscrivez à l'offre de SGI Mali pour bénéficier d'une expérience sur mesure et adaptée à vos besoins.",
-    images: ["https://sgimali-frontend.vercel.app/images/logo-og.png"],
-  },
-  manifest: "/site.webmanifest",
 };
 
-const descData = {
-  pageTitle: "Souscription",
-  description: [
-    "Lorem ipsum dolor, sit amet consectetur adipisicing elit. At aut ratione voluptatum, voluptate asperiores laborum ullam enim repellendus perspiciatis numquam adipisci deserunt dolorum quae perferendis optio animi cupiditate minus totam?",
-    "Lorem ipsum dolor, sit amet consectetur adipisicing elit. At aut ratione voluptatum, voluptate asperiores laborum ullam enim repellendus perspiciatis numquam adipisci deserunt dolorum quae perferendis optio animi cupiditate minus totam?"
-  ]
-};
+// Fonction pour récupérer les données de l'souscription
+async function getSouscription(): Promise<SouscriptionData[]> {
+  const apiUrl = "https://sgimali-frontend.vercel.app/api/pages?per_page=30";
+  const res = await fetch(apiUrl, {
+    next: { revalidate: 60 },
+  });
 
+  if (!res.ok) {
+    throw new Error("Failed to fetch nous contacter data");
+  }
 
-export default function Souscription() {
+  const pages = await res.json();
+  return pages.map((page: Page) => ({
+    title: page.title.rendered,
+    description: page.content.rendered, // Récupération du contenu HTML
+    image: page.featured_image_url || "", // Récupération de l'URL de l'image mise en avant
+    slug: page.slug,
+  }));
+}
+
+export default async function Souscription() {
+  const dataSouscription = await getSouscription();
+
+  if (!dataSouscription || dataSouscription.length === 0) {
+    return <SkeletonTemplatePages />;
+  }
+
+  
+  // Si une des pages a un slug égal à "souscription", cette page sera retournée par la méthode find() et assignée à la variable souscription.
+  const souscription = dataSouscription.find(
+    (page) => page.slug === "souscription"
+  );
+
+  if (!souscription) {
+    return <SkeletonHeaderPageSection />;
+  }
+
   return (
     <div>
-    
-      <HeaderPageSection title={"Souscription"} />
+      <HeaderPageSection title={souscription.title} />
 
       <section style={{ padding: "39px 0" }}>
-
-
         <div className="container">
-    <div className="row align-items-center">
-      {/* Bloc gauche : Texte */}
-      
-      
-      <div className="col-md-12">
-            <div className="title-block title-contac">
-            <SectionTitle title="Souscription" />
-            </div>
-          </div>
+          <div className="row align-items-center">
+            {/* Bloc gauche : Texte */}
 
-      
-      <div className="col-md-6">
-        <div className="main-page">
-            
-        <p style={{ fontSize: "14px", lineHeight: "1.8", color: "#555" }}>
-                  {descData.description.map((text, index) => (
-                    <span key={index}>
-                      {text}
-                      <br />
-                    </span>
-                  ))}
-                </p>
-                <br/>
-                <p>image ici</p>
+            <div className="col-md-12">
+              <div className="title-block title-contac">
+                <SectionTitle title={souscription.title} />
+              </div>
+            </div>
+
+            <div className="col-md-6">
+              <div
+                className="faq-description"
+                style={{ fontSize: 14, lineHeight: 1.8, color: "#555" }}
+                dangerouslySetInnerHTML={{ __html: souscription.description }} // Affichage du contenu HTML
+              />
+              <br />
+              <Image
+                src={souscription.image || "/images/default.webp"} // Source de l'image
+                alt="Faq SGI Mali" // Texte alternatif
+                className="img img-responsive"
+                style={{
+                  borderRadius: "8px",
+                  boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
+                }}
+                width={500}
+                height={300}
+                layout="intrinsic"
+              />
+              <br />
+            </div>
+            {/* Bloc droit : Image */}
+            <SubscriptionForm/>
+          </div>
         </div>
-      </div>
-      {/* Bloc droit : Image */}
-      <div className="col-md-6" style={{marginTop:-5}}>
-        <div className="main-page">
-        <form
-              name="subscriptionForm"
-              method="post"
-              action=""
-            >
-              {/* Je souhaite souscrire à l'offre */}
-              <div className="form-group">
-                <label htmlFor="offer">
-                Je souhaite souscrire à l&apos;offre{" "}
-                  <span className="text-danger">*</span>:
-                </label>
-                <input
-                  type="text"
-                  className="form-control"
-                  id="offer"
-                  name="offer"
-                  placeholder="Entrez l'offre"
-                />
-              </div>
-              {/* En tant que personne */}
-              <div className="form-group">
-                <label htmlFor="person">
-                  En tant que personne <span className="text-danger">*</span>:
-                </label>
-                <input
-                  type="text"
-                  className="form-control"
-                  id="person"
-                  name="person"
-                  placeholder="Entrez votre statut"
-                  required={true} 
-                />
-              </div>
-              {/* Nom */}
-              <div className="form-group">
-                <label htmlFor="lastName">
-                Je soussigné(e) Nom <span className="text-danger">*</span>:
-                </label>
-                <input
-                  type="text"
-                  className="form-control"
-                  id="lastName"
-                  name="last_name"
-                  placeholder="Entrez votre nom"
-                />
-              </div>
-              {/* Prénoms */}
-              <div className="form-group">
-                <label htmlFor="firstName">
-                  Prénoms <span className="text-danger">*</span>:
-                </label>
-                <input
-                  type="text"
-                  className="form-control"
-                  id="firstName"
-                  name="first_name"
-                  placeholder="Entrez vos prénoms"
-                />
-              </div>
-              {/* Lieu de Naissance */}
-              <div className="form-group">
-                <label htmlFor="birthPlace">
-                  Lieu de Naissance <span className="text-danger">*</span>:
-                </label>
-                <input
-                  type="text"
-                  className="form-control"
-                  id="birthPlace"
-                  name="birth_place"
-                  placeholder="Entrez votre lieu de naissance"
-                />
-              </div>
-              {/* Date de Naissance */}
-              <div className="form-group">
-                <label htmlFor="birthDate">
-                  Date de Naissance <span className="text-danger">*</span>:
-                </label>
-                <input
-                  type="date"
-                  className="form-control"
-                  id="birthDate"
-                  name="birth_date"
-                />
-              </div>
-              {/* Sexe */}
-              <div className="form-group">
-                <label>
-                  Sexe <span className="text-danger">*</span>:
-                </label>
-                <div>
-                  <label className="radio-inline">
-                    <input
-                      type="radio"
-                      name="gender"
-                      defaultValue="Masculin"
-                    />{" "}
-                    Masculin
-                  </label>
-                  <label className="radio-inline">
-                    <input
-                      type="radio"
-                      name="gender"
-                      defaultValue="Féminin"
-                    />{" "}
-                    Féminin
-                  </label>
-                </div>
-              </div>
-              {/* Pièce d'identité */}
-              <div className="form-group">
-                <label htmlFor="identityType">
-                Pièce d&apos;identité <span className="text-danger">*</span>:
-                </label>
-                <select
-                  className="form-control"
-                  id="identityType"
-                  name="identity_type"
-                >
-                  <option value="">Sélectionnez</option>
-                  <option value="CNI">CNI</option>
-                  <option value="Passeport">Passeport</option>
-                  <option value="Autre">Autre (à préciser)</option>
-                </select>
-              </div>
-              {/* Numéro de pièce d'identité */}
-              <div className="form-group">
-                <label htmlFor="identityNumber">
-                Numéro de pièce d&apos;identité:
-                </label>
-                <input
-                  type="text"
-                  className="form-control"
-                  id="identityNumber"
-                  name="identity_number"
-                  placeholder="Entrez le numéro"
-                />
-              </div>
-              {/* Délivré le */}
-              <div className="form-group">
-                <label htmlFor="issuedDate">
-                  Délivré le <span className="text-danger">*</span>:
-                </label>
-                <input
-                  type="date"
-                  className="form-control"
-                  id="issuedDate"
-                  name="issued_date"
-                />
-              </div>
-              {/* Par */}
-              <div className="form-group">
-                <label htmlFor="issuedBy">
-                  Par <span className="text-danger">*</span>:
-                </label>
-                <input
-                  type="text"
-                  className="form-control"
-                  id="issuedBy"
-                  name="issued_by"
-                  placeholder="Autorité délivrante"
-                />
-              </div>
-              {/* Adresse postale */}
-              <div className="form-group">
-                <label htmlFor="postalAddress">
-                  Adresse postale <span className="text-danger">*</span>:
-                </label>
-                <input
-                  type="text"
-                  className="form-control"
-                  id="postalAddress"
-                  name="postal_address"
-                  placeholder="Entrez votre adresse"
-                />
-              </div>
-              {/* Localité */}
-              <div className="form-group">
-                <label htmlFor="locality">
-                  Localité <span className="text-danger">*</span>:
-                </label>
-                <input
-                  type="text"
-                  className="form-control"
-                  id="locality"
-                  name="locality"
-                  placeholder="Entrez votre localité"
-                />
-              </div>
-              {/* Numéro de téléphone */}
-              <div className="form-group">
-                <label htmlFor="phoneNumber">
-                  Numéro de téléphone <span className="text-danger">*</span>:
-                </label>
-                <input
-                  type="tel"
-                  className="form-control"
-                  id="phoneNumber"
-                  name="phone_number"
-                  placeholder="Format 00xxxxx"
-                />
-              </div>
-              {/* Fax */}
-              <div className="form-group">
-                <label htmlFor="fax">Fax:</label>
-                <input
-                  type="text"
-                  className="form-control"
-                  id="fax"
-                  name="fax"
-                  placeholder="Entrez votre fax"
-                />
-              </div>
-              {/* Email */}
-              <div className="form-group">
-                <label htmlFor="email">
-                  Email <span className="text-danger">*</span>:
-                </label>
-                <input
-                  type="email"
-                  className="form-control"
-                  id="email"
-                  name="email"
-                  placeholder="Entrez votre email"
-                />
-              </div>
-              {/* Submit Button */}
-              <div style={{float:'right'}}>
-                <button type="submit" className="btn-main-color" style={{borderRadius:4, fontWeight:400, fontSize:15, paddingLeft:25, paddingRight:25, paddingTop:7, paddingBottom:7}}>            
-            Envoyer
-          </button>                    
-              </div>
-              
-            </form>
-        </div>
-      </div>
-    </div>
-  </div>
       </section>
     </div>
   );
---
+++ src/components/SubscriptionForm.tsx
@@ -0,0 +1,261 @@
+"use client";
+
+import { useState } from "react";
+import Swal from "sweetalert2";
+import { Bars } from "react-loader-spinner";
+
+const SubscriptionForm = () => {
+
+  interface FormData {
+    offer: string;
+    person: string;
+    last_name: string;
+    first_name: string;
+    birth_place: string;
+    birth_date: string;
+    gender: string;
+    identity_type: string;
+    identity_number: string;
+    issued_date: string;
+    issued_by: string;
+    postal_address: string;
+    locality: string;
+    phone_number: string;
+    fax: string;
+    email: string;
+    // Ajoutez d'autres champs si nécessaire
+  }
+
+  const [formData, setFormData] = useState<FormData>({
+    offer: "",
+    person: "",
+    last_name: "",
+    first_name: "",
+    birth_place: "",
+    birth_date: "",
+    gender: "",
+    identity_type: "",
+    identity_number: "",
+    issued_date: "",
+    issued_by: "",
+    postal_address: "",
+    locality: "",
+    phone_number: "",
+    fax: "",
+    email: "",
+  });
+
+  const [loading, setLoading] = useState(false);
+  const [formDisabled, setFormDisabled] = useState(false);
+
+  const fieldLabels: { [key: string]: string } = {
+    offer: "Je souhaite souscrire à l'offre",
+    person: "En tant que personne",
+    last_name: "Je soussigné(e) Nom",
+    first_name: "Prénom(s)",
+    birth_place: "Lieu de naissance",
+    birth_date: "Date de naissance",
+    gender: "Genre",
+    identity_type: "Type de pièce d'identité",
+    identity_number: "Numéro de pièce d'identité",
+    issued_date: "Délivré le",
+    issued_by: "par",
+    postal_address: "Votre adresse postale",
+    locality: "Votre localité",
+    phone_number: "Votre numéro de téléphone",
+    fax: "Fax",
+    email: "E-mail",
+  };
+
+  
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    // Validation des champs obligatoires
+    const requiredFields: (keyof typeof formData)[] = [
+      "offer",
+      "person",
+      "last_name",
+      "first_name",
+      "birth_place",
+      "birth_date",
+      "gender",
+      "identity_type",
+      "identity_number",
+      "issued_date",
+      "issued_by",
+      "postal_address",
+      "locality",
+      "phone_number",
+      "email",
+    ];
+
+    const missingFields = requiredFields.filter((field) => !formData[field]);
+    if (missingFields.length > 0) {
+      Swal.fire({
+        icon: "error",
+        title: "Erreur",
+        text: "Tous les champs obligatoires doivent être remplis.",
+      });
+      return;
+    }
+
+    setLoading(true);
+    setFormDisabled(true);
+
+    try {
+      // Enregistrement dans la base
+      const apiUrl =
+        "https://sgi.cynomedia-africa.com/wp-json/subscription-api/v1/add-subscriber";
+      const response = await fetch(apiUrl, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+
+      if (!response.ok) {
+        throw new Error("Erreur lors de l'enregistrement.");
+      }
+
+      // Envoi de la notification par e-mail
+      const emailApiUrl =
+        "https://sgi.cynomedia-africa.com/wp-json/custom/v1/send-subscription-notification/";
+
+      const emailResponse = await fetch(emailApiUrl, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          first_name: formData.first_name,
+          email: formData.email,
+          subject: "Nouveau Souscripteur",
+          message: "Nouveau Souscripteur",//Bonjour ${formData.first_name},\n\nVotre inscription a été enregistrée avec succès.\n\nCordialement.
+        }),
+      });
+
+      if (!emailResponse.ok) {
+        throw new Error("Erreur lors de l'envoi de l'email.");
+      }
+
+      Swal.fire({
+        icon: "success",
+        title: "Succès",
+        text: "Enregistrement effectué avec succès !",
+      });
+
+      setFormData({
+        offer: "",
+        person: "",
+        last_name: "",
+        first_name: "",
+        birth_place: "",
+        birth_date: "",
+        gender: "",
+        identity_type: "",
+        identity_number: "",
+        issued_date: "",
+        issued_by: "",
+        postal_address: "",
+        locality: "",
+        phone_number: "",
+        fax: "",
+        email: "",
+      });
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        Swal.fire({
+          icon: "error",
+          title: "Erreur",
+          text: error.message,
+        });
+      }else {
+        Swal.fire({
+          icon: "error",
+          title: "Erreur",
+          text: "Une erreur inconnue est survenue.",
+        });
+      }
+    } finally {
+      setLoading(false);
+      setFormDisabled(false);
+    }
+  };
+
+
+  
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+// Fonction pour capitaliser la première lettre en minuscule sauf pour la première
+function capitalizeFirstLetter(string: string): string {
+  if (!string) return ""; // Gestion des chaînes vides ou nulles
+  return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+}
+
+  return (
+    <div className="col-md-6" style={{ marginTop: -5 }}>
+      <div className="main-page">
+        <form onSubmit={handleSubmit}>
+          {loading && (
+            <div style={{ textAlign: "center" }}>
+              <Bars
+                height={50}
+                width={50}
+                color="#4CAF50"
+                visible={true}
+                ariaLabel="Loading..."
+              />
+            </div>
+          )}
+
+          {/* Champs de formulaire avec labels en français et première lettre en majuscule */}
+          {Object.keys(formData).map((field) => (
+            <div className="form-group" key={field}>
+              <label htmlFor={field}>
+                      {fieldLabels[field] || capitalizeFirstLetter(field.replace(/_/g, " "))} <span className="text-danger">*</span>
+              </label>
+              <input
+                type={field.includes("date") ? "date" : "text"}
+                className="form-control"
+                id={field}
+                name={field}                
+                value={formData[field as keyof FormData]} // Utilisation de l'indexation sécurisée
+                onChange={handleChange}
+                disabled={formDisabled}
+              />
+            </div>
+          ))}
+
+          <button
+            type="submit"
+            className="btn-main-color"
+              style={{
+                borderRadius: 4,
+                fontWeight: 400,
+                fontSize: 15,
+                paddingLeft: 25,
+                paddingRight: 25,
+                paddingTop: 7,
+                paddingBottom: 7,
+                float:"right"
+              }}
+            disabled={formDisabled}
+          >
+            Envoyer
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default SubscriptionForm;
