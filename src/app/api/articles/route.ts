4779d07664d2099163c203c46b73159387c95921
devlewi <lewimarcjean3@gmail.com>
2024-12-19 15:22:42 +0000

    update proxy6

--- src/app/api/articles/route.ts
+++ src/app/api/articles/route.ts
@@ -45,12 +45,14 @@ export async function GET(request: NextRequest) {
 
     // Retourner les données sous forme de réponse JSON à l'utilisateur
     return NextResponse.json(responseData);
-
-  } catch (error) {
-    console.error("Error fetching posts:", error);
+} catch (error: unknown) {
+    // Forcer TypeScript à traiter 'error' comme une instance de Error
+    const e = error as Error;
+    console.error("Error fetching posts:", e);
     return NextResponse.json(
-      { message: "Failed to fetch posts", error: error.message },
+      { message: "Failed to fetch posts", error: e.message },
       { status: 500 }
     );
   }
+  
 }
