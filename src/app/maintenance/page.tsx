35699e324cc640a7be6b29b17bb02a1287c3b3bd
devlewi <lewimarcjean3@gmail.com>
2025-01-10 07:10:57 +0000

    change backend with prod 4

--- src/app/maintenance/page.tsx
+++ src/app/maintenance/page.tsx
@@ -5,7 +5,7 @@ export default function MaintenancePage() {
     <div className={styles.container}>
       <h1 className={styles.title}>Site en Maintenance</h1>
       <p className={styles.message}>
-        Notre site est actuellement en maintenance pour des mises à jour importantes. 
+        Notre site est actuellement en maintenance pour des mises à jour importantes . 
       </p>
       <p className={styles.message}>
         Il sera à nouveau disponible dans peu de temps.
