a61a52bfb6bc856a467c54eb9364752fdd4e8f4e
devlewi <lewimarcjean3@gmail.com>
2026-08-28 14:18:17 +0000

    enable website - disable middleware

--- src/middleware.tsx
+++ src/middleware.tsx
@@ -2,24 +2,8 @@ import { NextResponse } from "next/server";
 
 export default function middleware() {
   // Renvoie directement un document HTML blanc
-  return new NextResponse(
-    `<!DOCTYPE html>
-    <html lang="fr">
-      <head>
-        <meta charset="utf-8" />
-        <title>SGI Mali</title>
-        <meta name="robots" content="noindex, nofollow" />
-      </head>
-      <body style="background-color: white; margin: 0; padding: 0;"></body>
-    </html>`,
-    {
-      status: 200,
-      headers: {
-        "content-type": "text/html; charset=utf-8",
-        "cache-control": "no-store, max-age=0",
-      },
-    }
-  );
+    return NextResponse.next();
+
 }
 
 export const config = {
