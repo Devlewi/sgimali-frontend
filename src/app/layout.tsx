9c53aa7325e404d5ff1937e732b398e6cfc9fa55
devlewi <lewimarcjean3@gmail.com>
2026-09-04 13:37:20 +0000

    disable correction

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,9 @@
-/* eslint-disable @typescript-eslint/ban-ts-comment */
-// @ts-ignore
 //import "../globals.css";
 
-type Props = {
-  params: Promise<{ locale: string }>;
-  children: React.ReactNode;
-};
-
-export default async function RootLayout({ params }: Props) {
-  const { locale } = await params;
-
+export default function RootLayout() {
   return (
-    <html lang={locale}>
-      <body className="bg-white min-h-screen">
-        {/* On ne rend rien ici pour simuler la page blanche */}
-      </body>
+    <html lang="fr">
+      <body className="bg-white min-h-screen"></body>
     </html>
   );
 }
